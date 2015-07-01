c05f1f95df7b3aac1839f5733141e33991952307
Wilhelm Behncke <behncke@sitegeist.de>
2015-07-01 17:07:46 +0200

    [TASK] Implement create operation & add PageTreeMixin
    Releases: master

--- Resources/Private/JavaScript/Apps/Collection/App.js
+++ Resources/Private/JavaScript/Apps/Collection/App.js
@@ -1,4 +1,6 @@
 let React = require('react');
+let modalView;
+
 class Collection {
 
   constructor(el, dic) {
@@ -9,32 +11,38 @@ class Collection {
 
     this.dic.SharedScope.expect('Sitegeist.NeosBackendBridge:Controller.FooterPanel')
       .then(this.initializeFooterPanel.bind(this));
+
+    [].forEach.call(this.el.querySelectorAll('input[data-namespace="Sitegeist.CriticalMass:Collection"]'), (input) => {
+      if (input.dataset.key && input.value) {
+        this[ input.dataset.key ] = input.value;
+      }
+    });
   }
 
   initializeFooterPanel(footerPanel) {
     this.footerPanelView || (this.footerPanelView = require('./Views/FooterPanel.jsx').Element(this));
-    console.log(this.footerPanelView);
-    footerPanel.pushContent('CriticalMass', this.footerPanelView);
+    footerPanel.pushContent(this.footerPanelView);
   }
 
   createNode() {
-    // Open Modal Dialog
     let data;
     this.dic.SharedScope.expect('Sitegeist.NeosBackendBridge:Controller.Modal')
-      .then((Modal) => {
-        this.modalView || (this.modalView = require('./Views/Modal.jsx').Component);
-        let modal = new Modal(this.modalView);
-
-        console.log(modal);
-
-        return modal.promptResult();
-      }).then(function(result) {
+      .then((Modal) => { // Open Modal Dialog
+        modalView || (modalView = require('./Views/Modal.jsx').Component);
+        return Modal.prompt('Create new Entry', modalView);
+      }).then((result) => { // When there is a modal result, try to load NeosServiceAPI
         data = result;
         return this.dic.SharedScope.expect('TYPO3.Neos:NeosServiceAPI');
-      }).then((NeosServiceAPI) => {
-
+      }).then((NeosServiceAPI) => { // With NeosServiceAPI: Create node
+        return NeosServiceAPI.createNode(this.referenceNode, {
+          nodeType: this.nodeType,
+          properties: data
+        }, this.insertPosition || 'into');
+      }).then((result) => { // if everything is in place, redirect to the resulting page
+        window.location.href = result.data.nextUri;
       })
-      .catch((reason) => {
+      .catch((reason) => { // Handle Errors
+        console.log(reason);
         if (reason instanceof Error) {
           // Handle error
         }
