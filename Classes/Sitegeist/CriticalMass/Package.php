1f1ca898617b90c45b07e53645c420ea41cc0df8
Martin Ficzel <ficzel@sitegeist.de>
2016-06-07 14:31:56 +0200

    TASK: Refactor the coding style to psr2 and remove the separate configuration of the TypoScript Context

--- Classes/Sitegeist/CriticalMass/Package.php
+++ Classes/Sitegeist/CriticalMass/Package.php
@@ -4,14 +4,17 @@ namespace Sitegeist\CriticalMass;
 
 use TYPO3\Flow\Package\Package as BasePackage;
 
-
 /**
  * The Sitegeist\CriticalMass
  */
-class Package extends BasePackage {
-    public function boot(\TYPO3\Flow\Core\Bootstrap $bootstrap) {
+class Package extends BasePackage
+{
+    public function boot(\TYPO3\Flow\Core\Bootstrap $bootstrap)
+    {
         $dispatcher = $bootstrap->getSignalSlotDispatcher();
-        $dispatcher->connect('TYPO3\TYPO3CR\Domain\Model\Node', 'nodeCreated', 'Sitegeist\CriticalMass\Hooks\ContentRepositoryHooks', 'nodeCreated');
-        $dispatcher->connect('TYPO3\TYPO3CR\Domain\Model\Node', 'nodeUpdated', 'Sitegeist\CriticalMass\Hooks\ContentRepositoryHooks', 'nodeUpdated');
+        $dispatcher->connect('TYPO3\TYPO3CR\Domain\Model\Node', 'nodeCreated',
+            'Sitegeist\CriticalMass\Hooks\ContentRepositoryHooks', 'nodeCreated');
+        $dispatcher->connect('TYPO3\TYPO3CR\Domain\Model\Node', 'nodeUpdated',
+            'Sitegeist\CriticalMass\Hooks\ContentRepositoryHooks', 'nodeUpdated');
     }
 }
