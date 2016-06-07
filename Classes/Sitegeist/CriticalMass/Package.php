3d3ce2aaf208013a7100838b499cf72788a7e70b
Martin Ficzel <ficzel@sitegeist.de>
2016-06-07 13:52:49 +0200

    TASK: Remove the traces of the old critical mass codebase

--- Classes/Sitegeist/CriticalMass/Package.php
+++ Classes/Sitegeist/CriticalMass/Package.php
@@ -9,11 +9,9 @@ use TYPO3\Flow\Package\Package as BasePackage;
  * The Sitegeist\CriticalMass
  */
 class Package extends BasePackage {
-
     public function boot(\TYPO3\Flow\Core\Bootstrap $bootstrap) {
         $dispatcher = $bootstrap->getSignalSlotDispatcher();
         $dispatcher->connect('TYPO3\TYPO3CR\Domain\Model\Node', 'nodeCreated', 'Sitegeist\CriticalMass\Hooks\ContentRepositoryHooks', 'nodeCreated');
         $dispatcher->connect('TYPO3\TYPO3CR\Domain\Model\Node', 'nodeUpdated', 'Sitegeist\CriticalMass\Hooks\ContentRepositoryHooks', 'nodeUpdated');
-
     }
 }
--- Gulpfile.js
+++
@@ -1,7 +0,0 @@
-var build = require('./Build/Index.js');
-
-// Add package your configurations here.
-// The tasks for each package will be automatically created.
-build.addPackages([
-    require('./PackageConfig.js')
-]);
